d86ee245dc3871e7d9cf9ac47477729c3415e0be
weiheng5672 <143916114+weiheng5672@users.noreply.github.com>
2024-08-23 21:12:46 +0800

    新增長條圖

--- app/src/main/java/com/example/weatherretrofit/ui/BarChatScreen.kt
+++ app/src/main/java/com/example/weatherretrofit/ui/BarChatScreen.kt
@@ -6,14 +6,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
