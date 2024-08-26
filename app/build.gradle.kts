fac3730e68efd496ae8e175b596778acaa5cf9c6
weiheng5672 <143916114+weiheng5672@users.noreply.github.com>
2024-08-26 19:57:03 +0800

    恢復單一版本安裝

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,21 +22,6 @@ android {
         }
     }
 
-    // 添加 flavor 维度
-    flavorDimensions.add("version2")
-
-    // 定义不同的 product flavors
-    productFlavors {
-        create("dev") {
-            dimension = "version2"
-            applicationIdSuffix = ".dev"
-            versionNameSuffix = "-dev"
-        }
-        create("prod") {
-            dimension = "version2"
-        }
-    }
-
     buildTypes {
         release {
             isMinifyEnabled = false
